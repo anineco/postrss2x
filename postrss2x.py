a2b2e5d79c8ad28bcbd5513af11002794cfc59f9
Anineco Nyanta <anineco@nifty.com>
2025-01-13 21:56:58 +0900

    Comment, messages in English

--- postrss2x.py
+++ postrss2x.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# RSS から X (Twetter) に記事を自動投稿する
+# Automatically post articles from RSS to X (Twitter)
 
 import os
 from email.utils import parsedate_to_datetime
@@ -12,24 +12,24 @@ import xml.etree.ElementTree as ET
 import requests
 import tweepy
 
-import config # config.py に認証情報を記述
+import config # add your API keys and RSS URL to config.py
 
-# コマンドライン引数
+# command line options
 post = False
 disp = False
 mark = False
 for arg in sys.argv[1:]:
     if arg == "-p":
-        post = True # Xに投稿する
+        post = True # post articles
     elif arg == "-l":
-        disp = True # 記事の一覧を表示する
+        disp = True # list articles
     elif arg == "-m":
-        mark = True # 記事を全て投稿済にする
+        mark = True # mark all articles as posted
     else:
         print(f"Usage: {sys.argv[0]} [-p] [-l] [-m]")
         sys.exit(1)
 
-# X API v2 認証設定
+# X API v2 credentials
 client = tweepy.Client(
     consumer_key=config.API_KEY,
     consumer_secret=config.API_KEY_SECRET,
@@ -37,7 +37,7 @@ client = tweepy.Client(
     access_token_secret=config.ACCESS_TOKEN_SECRET
 )
 
-# 配信記録データベース
+# database for recording posted articles
 script_path = os.path.abspath(__file__)
 script_dir = os.path.dirname(script_path)
 os.chdir(script_dir)
@@ -54,13 +54,13 @@ CREATE TABLE IF NOT EXISTS record (
 """)
 connection.commit()
 
-# RSSを取得
+# retrieve updated articles from RSS
 response = requests.get(config.RSS_URL)
 if not response.status_code == 200:
     print("Can't get RSS: ", response.status_code)
     sys.exit(1)
 
-# RSSを解析して、記事情報をデータベースに保存
+# parse RSS feed and insert URL, title, and publication date into the database
 root = ET.fromstring(response.content)
 namespaces = { "dc": "http://purl.org/dc/elements/1.1/"}
 version = root.get("version")
@@ -68,16 +68,16 @@ if not version == "2.0":
     # RSS 1.0
     namespaces[""] = "http://purl.org/rss/1.0/"
 for item in root.findall(".//item", namespaces):
-    link = item.find("link", namespaces).text   # 記事のURL
-    title = item.find("title", namespaces).text # タイトル
+    link = item.find("link", namespaces).text   # URL of the article
+    title = item.find("title", namespaces).text # title
     if version == "2.0" and (p := item.find("pubDate", namespaces)) is not None:
         date = parsedate_to_datetime(p.text).isoformat()
     else:
-        date = item.find("dc:date", namespaces).text # 公開日 (ISO8601)
+        date = item.find("dc:date", namespaces).text # publication date (ISO8601)
     cursor.execute("INSERT OR IGNORE INTO record (link,title,date) VALUES (?,?,?)", (link, title, date))
     connection.commit()
 
-# 日付順で上位 MAX_RECORDS 件を残して削除
+# delete old records except for the latest MAX_RECORDS
 cursor.execute("""
 DELETE FROM record WHERE link NOT IN (
     SELECT link
@@ -88,18 +88,18 @@ DELETE FROM record WHERE link NOT IN (
 """, (config.MAX_RECORDS,))
 connection.commit()
 
-# 記事を全て投稿済にする
 if mark:
+    # mark all articles as posted
     cursor.execute("UPDATE record SET post_id=0 WHERE post_id<0")
     connection.commit()
 
 if disp:
-    # 一覧表示
+    # list all articles in the database
     cursor.execute("SELECT link,title,date,post_id FROM record ORDER BY date ASC")
     for link, title, date, post_id in cursor.fetchall():
         print(date, link, title, f"({post_id})")
 
-# 未投稿で最も古い記録から MAX_POSTS 件を取得
+# find articles not yet posted ordered by publication date
 cursor.execute("SELECT link,title FROM record WHERE post_id<0 ORDER BY date ASC LIMIT ?", (config.MAX_POSTS,))
 for row in cursor.fetchall():
     message = config.MESSAGE.format(link=row[0], title=row[1])
@@ -108,10 +108,10 @@ for row in cursor.fetchall():
         try:
             response = client.create_tweet(text=message)
         except Exception as e:
-            print(f"投稿エラー: {e}")
+            print(f"Failed to post. Error: {e}") 
             sys.exit(1)
         post_id = response.data["id"]
-        print(f"投稿成功: {post_id}")
+        print(f"Success post: {post_id}")
         cursor.execute("UPDATE record SET post_id=? WHERE link=?", (post_id, link))
         connection.commit()
 
